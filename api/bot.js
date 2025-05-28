d5602cc0b3840d5b30ff0a84eafe0994a2689364
chastroman <chastroman@gmail.com>
2025-05-29 02:05:44 +0300

    Create bot.js

---
+++ api/bot.js
@@ -0,0 +1,25 @@
+const { Telegraf } = require('telegraf');
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.command('start', (ctx) => {
+  ctx.reply("👋 Привет! Нажми кнопку ниже, чтобы открыть сделку:", {
+    reply_markup: {
+      inline_keyboard: [[
+        {
+          text: 'Открыть Честно',
+          web_app: { url: 'https://chestno.online' }
+        }
+      ]]
+    }
+  });
+});
+
+module.exports = async (req, res) => {
+  try {
+    await bot.handleUpdate(req.body);
+  } catch (e) {
+    console.error(e);
+  }
+  res.end('ok');
+};
